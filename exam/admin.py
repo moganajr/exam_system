4f33d5190f07b0c947cedd6793de85159462cbb9
Mogana S. Flomo <moganajr@gmail.com>
2026-01-05 19:04:47 +0000

    Added CIU Admin Dashboard Export features

--- exam/admin.py
+++ exam/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from .models import ExamResult
 
-# Register your models here.
+
+@admin.register(ExamResult)
+class ExamResultAdmin(ImportExportModelAdmin):
+    list_display = (
+        "student_name",
+        "student_email",
+        "subject",
+        "score",
+        "total",
+        "percentage",
+        "status",
+        "created_at",
+    )
+
+    list_filter = (
+        "subject",
+        "status",
+        "created_at",
+    )
+
+    search_fields = (
+        "student_name",
+        "student_email",
+    )
+
+    ordering = ("-created_at",)
+    list_per_page = 25
--- exam_system/settings.py
+++ exam_system/settings.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'exam',
+    'import_export',
+
 ]
 
 

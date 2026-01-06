5c45736f0d8f170a1fd50690bc574b39a2d0b267
Mogana S. Flomo <moganajr@gmail.com>
2026-01-06 17:28:27 +0000

    Fix verify payment URL routing

--- exam/urls.py
+++ exam/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.verify_payment, name="verify_payment"),
+    path("verify-payment/", views.verify_payment, name="verify_payment"),
     path("select/", views.select_exam, name="select_exam"),
     path("english/", views.english_exam, name="english_exam"),
     path("math/", views.math_exam, name="math_exam"),

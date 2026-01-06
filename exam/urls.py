4458082d47d1e886f8fd66b7ab50ef839e003483
Mogana S. Flomo <moganajr@gmail.com>
2026-01-06 22:38:42 +0000

    Add root redirect to verify payment

--- exam/urls.py
+++ exam/urls.py
@@ -1,9 +1,13 @@
+from django.shortcuts import redirect
+from django.contrib import admin
 from django.urls import path
-from . import views
+from exam import views
 
 urlpatterns = [
-    path("verify-payment/", views.verify_payment, name="verify_payment"),
-    path("select/", views.select_exam, name="select_exam"),
-    path("english/", views.english_exam, name="english_exam"),
-    path("math/", views.math_exam, name="math_exam"),
+    path('', lambda request: redirect('verify_payment'), name='home'),
+    path('admin/', admin.site.urls),
+    path('verify-payment/', views.verify_payment, name='verify_payment'),
+    path('select/', views.select_exam, name='select_exam'),
+    path('english/', views.english_exam, name='english_exam'),
+    path('math/', views.math_exam, name='math_exam'),
 ]
--- exam_system/urls.py
+++ exam_system/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.shortcuts import redirect
+from exam import views
 
 urlpatterns = [
+    path('', lambda request: redirect('verify_payment'), name='home'),
+
     path('admin/', admin.site.urls),
-    path('', include('exam.urls')),
+
+    path('verify-payment/', views.verify_payment, name='verify_payment'),
+
+    path('select/', views.select_exam, name='select_exam'),
+
+    path('english/', views.english_exam, name='english_exam'),
+
+    path('math/', views.math_exam, name='math_exam'),
 ]
